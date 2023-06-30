284d26fc1b1fcf0466a8e383669df32912a85362
Naveen Dharshanala <113257029+naveendharshanala@users.noreply.github.com>
2023-06-30 21:35:18 +0530

    Add files via upload

---
+++ Age_and_Gender_Detection_Project/app.py
@@ -0,0 +1,77 @@
+import cv2 as cv
+import time
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+
+def getFaceBox(net, frame, conf_threshold=0.7):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn, bboxes
+
+faceProto = "models/opencv_face_detector.pbtxt"
+faceModel = "models/opencv_face_detector_uint8.pb"
+ageProto = "models/age_deploy.prototxt"
+ageModel = "models/age_net.caffemodel"
+genderProto = "models/gender_deploy.prototxt"
+genderModel = "models/gender_net.caffemodel"
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList = ['Male', 'Female']
+ageNet = cv.dnn.readNet(ageModel, ageProto)
+genderNet = cv.dnn.readNet(genderModel, genderProto)
+faceNet = cv.dnn.readNet(faceModel, faceProto)
+padding = 20
+
+def age_gender_detector(frame):
+    t = time.time()
+    frameFace, bboxes = getFaceBox(faceNet, frame)
+    for bbox in bboxes:
+        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+        blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
+        ageNet.setInput(blob)
+        agePreds = ageNet.forward()
+        age = ageList[agePreds[0].argmax()]
+        label = "{},{}".format(gender, age)
+        cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
+    return frameFace
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/'
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route("/uploader" , methods=['GET', 'POST'])
+def uploader():    
+    if request.method=='POST':
+        f = request.files['file1']
+        f.filename = "image.jpg"
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        input = cv.imread("static/image.jpg")
+        output = age_gender_detector(input)
+        cv.imwrite("static/output.jpg", output)
+        pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'output.jpg')
+        pic2 = os.path.join(app.config['UPLOAD_FOLDER'], 'image.jpg')
+        return render_template("uploaded.html", output_image=pic1, input_image=pic2)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
